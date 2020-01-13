8e5ad590b9730b4dc44cd88db181bb6de6aeed15
Jyneda <jyneda@gmail.com>
2020-01-14 01:00:19 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+let ADDR_IP_CAM = "CamIP";  //camera IP
+let PORT_IP_CAM_API = "CamPort";  //camera HTTP port 
+let VIDEO_FILE_NAME = "MJPG file name"; //MJPG file name
+
+let ADDR_iframe1 = "URL1"; //Remote control
+let height_iframe1 = "70%"; 
+let ADDR_iframe2 = "URL1"; //Dashboard webpage
+let height_iframe2 = "57%";
